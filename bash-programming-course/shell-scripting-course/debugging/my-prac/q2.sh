316fca16213cef5719b2c2c3bb720c47887aa52d
Ubuntu <ubuntu@ip-172-31-46-95.ap-south-1.compute.internal>
2025-03-11 07:51:26 +0000

    Added the debuging prac codes

---
+++ bash-programming-course/shell-scripting-course/debugging/my-prac/q2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+#
+
+ls "/home/ubuntu/Shell_Udemy_files/bash-programming-course/shell-scripting-course/debugging/my-prac"
+
+ls "/prac/"
+
+ls "/home/ubuntu/Shell_Udemy_files/bash-programming-course/shell-scripting-course/debugging/practice-exercises"
